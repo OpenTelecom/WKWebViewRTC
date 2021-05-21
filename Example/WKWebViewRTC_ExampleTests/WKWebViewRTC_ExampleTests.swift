5708debe5053a910cdcf8b76550ff2916c4f2a8c
Harold Thetiot <hthetiot@gmail.com>
2021-05-21 17:02:26 +0200

    implement basic WKWebViewRTC_ExampleTests bundle

---
+++ Example/WKWebViewRTC_ExampleTests/WKWebViewRTC_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  WKWebViewRTC_ExampleTests.swift
+//  WKWebViewRTC_ExampleTests
+//
+//  Created by Harold Thétiot on 21/05/2021.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class WKWebViewRTC_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
