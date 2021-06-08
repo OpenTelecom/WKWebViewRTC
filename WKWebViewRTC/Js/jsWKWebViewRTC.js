72703b81a02377f88fd195a7afd232b8f6efbf23
Noah Mehl <noahmehl@gmail.com>
2021-06-08 10:04:28 -0400

    Remove ReplaceTrack

--- WKWebViewRTC/Js/jsWKWebViewRTC.js
+++ WKWebViewRTC/Js/jsWKWebViewRTC.js
@@ -4025,7 +4025,6 @@ module.exports = RTCRtpReceiver;
 function RTCRtpReceiver(data) {
 	data = data || {};
 
-	this._pc = data.pc;
 	this.track = data.track;
 }
 
@@ -4047,7 +4046,6 @@ module.exports = RTCRtpSender;
 function RTCRtpSender(data) {
 	data = data || {};
 
-    this._pc = data.pc;
 	this.track = data.track;
     this.params = data.params || {};
 }
@@ -4061,30 +4059,6 @@ RTCRtpSender.prototype.setParameters = function (params) {
     return Promise.resolve(this.params);
 };
 
-RTCRtpSender.prototype.replaceTrack = function (withTrack) {
-	var self = this,
-		pc = self._pc;
-
-	return new Promise(function (resolve, reject) {
-		pc.removeTrack(self);
-		pc.addTrack(withTrack);
-		self.track = withTrack;
-
-		// https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/negotiationneeded_event
-		var event = new Event('negotiationneeded');
-		pc.dispatchEvent('negotiationneeded', event);
-
-		pc.addEventListener("signalingstatechange", function listener() {
-			if (pc.signalingState === "closed") {
-				pc.removeEventListener("signalingstatechange", listener);
-				reject();
-			} else if (pc.signalingState === "stable") {
-				pc.removeEventListener("signalingstatechange", listener);
-				resolve();
-			}
-		});
-	});
-};
 },{}],26:[function(require,module,exports){
 /*
  * cordova-plugin-iosrtc v6.0.17
